f1b4357b0ccb1fff6c81095e3730bcc4bbf44522
drumath <2237k.drumath@gmail.com>
2020-12-28 16:34:39 +0900

    :sparkles: ホットリロードの動作確認

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+  const dom = document.getElementById("message");
+  if (dom != null) {
+    dom.innerHTML = "Hello!!";
+  }
+});
---
+++ webpack.config.ts
@@ -0,0 +1,40 @@
+// const path = require('path');
+import * as path from "path";
+import * as webpack from "webpack";
+import "webpack-dev-server";
+
+const config: webpack.Configuration[] = [
+  {
+    name: "local_dev",
+    mode: "development",
+    entry: "./src/index.ts",
+    devtool: "source-map",
+    output: {
+      filename: "bundle.js",
+      path: path.resolve(__dirname, "dist/src"),
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          exclude: /node_modules/,
+          loader: "ts-loader",
+        },
+      ],
+    },
+    devServer: {
+      open: false,
+      openPage: "index.html",
+      contentBase: path.resolve(__dirname, "dist"),
+      watchContentBase: true,
+      port: 8080,
+      publicPath: "/src/",
+    },
+    resolve: {
+      extensions: [".ts", ".js"],
+      modules: ["node_modules"],
+    },
+  },
+];
+
+export default config;
