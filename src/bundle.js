b5044e442fd3d1f4ae5dc15b744b6496e887b4f6
drumath2237 <drumath2237@users.noreply.github.com>
2020-12-28 08:12:54 +0000

    deploy: 3c215471c99b5261f49e6cbd89fba5b967ae6b08

---
+++ src/bundle.js
@@ -0,0 +1,16 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/*!**********************!*\
+  !*** ./src/index.ts ***!
+  \**********************/
+
+document.addEventListener("DOMContentLoaded", function (event) {
+    var dom = document.getElementById("message");
+    if (dom != null) {
+        dom.innerHTML = "Hello!!";
+    }
+});
+
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
